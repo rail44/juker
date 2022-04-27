30f8b3337895ca32903a3b5ba6e7bc7cef9ba69c
Satoshi Amemiya <amemiya@protonmail.com>
2022-04-28 01:56:44 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::Server;
+use std::net::SocketAddr;
+use tower_http::services::ServeDir;
+use tower::make::Shared;
+
+#[tokio::main]
+async fn main() {
+    // tokio::spawn(async {
+    //     let addr = SocketAddr::from(([127, 0, 0, 1], 8888));
+    //     let socket = TcpListener::bind(&addr).await.unwrap();
+    //     while let Ok((stream, _)) = socket.accept().await {
+    //         tokio_tungstenite::accept_async(stream).await.unwrap();
+    //     }
+    // });
+
+    let svc = Shared::new(ServeDir::new("src"));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    Server::bind(&addr).serve(svc).await.unwrap();
 }
