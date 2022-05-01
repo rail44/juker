1fa0d7bb94d27dfee24ba93cc654c037d57380cd
Satoshi Amemiya <amemiya@protonmail.com>
2022-05-01 23:35:13 +0900

    wip

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,24 @@
 use axum::{
-    routing::{get, post},
-    Router,
-    response::IntoResponse,
     http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    extract::Form,
+    Router,
 };
+use serde_json::json;
+use serde::Deserialize;
+use slack;
+use std::env;
 use std::net::SocketAddr;
+use tracing_subscriber;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     let app = Router::new()
         .route("/status", get(status))
-        .route("/command", post(create_user));
+        .route("/command", post(command));
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -22,6 +30,34 @@ async fn status() -> &'static str {
     "ok"
 }
 
-async fn create_user() -> impl IntoResponse {
-    (StatusCode::CREATED, "")
+#[derive(Deserialize)]
+struct CommandRequest {
+    trigger_id: String
+}
+
+async fn command(req: Form<CommandRequest>) -> impl IntoResponse {
+    let token = env::var("SLACK_TOKEN").unwrap();
+    let view = json!({
+        "type": "modal",
+        "title": {
+            "type": "plain_text",
+            "text": "Juker"
+        },
+        "blocks": [],
+    })
+    .to_string();
+    let trigger_id = &req.trigger_id;
+    println!("{trigger_id}");
+    println!("{view}");
+
+    slack::apis::views_api::views_open(
+        &slack::apis::configuration::Configuration::default(),
+        &token,
+        trigger_id,
+        &view,
+    )
+    .await
+    .unwrap();
+
+    StatusCode::OK
 }
